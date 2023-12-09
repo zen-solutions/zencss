66e8a310861d1bcd1ee9137efdea5697314fb2c3
“Shaun <djshamus@gmail.com>
2023-12-08 20:42:48 -0500

    fix bug with nested containers, bunch of docs work

--- js/modules/nested-containers.js
+++ js/modules/nested-containers.js
@@ -9,25 +9,56 @@
   // Nested containers - kill parent padding + if i am a row, and im inide a container that i neted in a column, then i need to have my martin top and bottom set to 0
   // BUG: Adding this functionality breaks convention with support for zentax and semantic. fix for semantic.
   //-------------------------------------
-  document.addEventListener('DOMContentLoaded', function () {
-    // Handle the padding for <z-col> elements
-    var cols = document.querySelectorAll('z-col');
 
-    cols.forEach(function (col) {
-        if (col.querySelector('z-container')) {
-            col.style.padding = '0';
-        }
+  document.addEventListener('DOMContentLoaded', function () {
+    // Handle the padding for <z-col> elements that directly contain <z-container>
+    var cols = document.querySelectorAll('z-col > z-container');
+
+    cols.forEach(function (container) {
+        // The parent of <z-container> is <z-col>
+        var col = container.parentElement;
+        col.style.padding = '0';
     });
 
     // Handle the margin for <z-row> elements within <z-container> inside <z-col>
     var rows = document.querySelectorAll('z-row');
     rows.forEach(function (row) {
-        // Use closest to check if <z-row> is inside <z-container> and that <z-container> is inside <z-col>
-        var container = row.closest('z-container');
-        if (container && container.parentElement.tagName.toLowerCase() === 'z-col') {
+        // Use closest to check if <z-row> is a direct child of <z-container> inside <z-col>
+        var container = row.closest('z-col > z-container');
+        if (container) {
             // Set the top and bottom padding of <z-row> to 0
             row.style.paddingTop = '0';
             row.style.paddingBottom = '0';
         }
     });
 });
+
+
+
+
+
+
+
+  //   document.addEventListener('DOMContentLoaded', function () {
+//     // Handle the padding for <z-col> elements
+//     var cols = document.querySelectorAll('z-col');
+
+//     cols.forEach(function (col) {
+//         if (col.querySelector('z-container')) {
+//             col.style.padding = '0';
+//             console.log("im the culprit");
+//         }
+//     });
+
+//     // Handle the margin for <z-row> elements within <z-container> inside <z-col>
+//     var rows = document.querySelectorAll('z-row');
+//     rows.forEach(function (row) {
+//         // Use closest to check if <z-row> is inside <z-container> and that <z-container> is inside <z-col>
+//         var container = row.closest('z-container');
+//         if (container && container.parentElement.tagName.toLowerCase() === 'z-col') {
+//             // Set the top and bottom padding of <z-row> to 0
+//             row.style.paddingTop = '0';
+//             row.style.paddingBottom = '0';
+//         }
+//     });
+// }); 
