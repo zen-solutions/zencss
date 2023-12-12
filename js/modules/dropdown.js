f99830949230884abaa2181642a248e054f7fcfe
“Shaun <djshamus@gmail.com>
2023-12-12 00:07:58 -0500

    fix dropdown

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,54 +1,42 @@
 /*
-* zenCSS v1.0.0 (https://zencss.com/)
-* Copyright 2023-2023 Shaun Mackey
-* Licensed under MIT (https://github.com/zen-solutions/zencss/blob/main/LICENSE)
-*/
- 
-  //-------------------------------------
-  // Dropdown
-  //-------------------------------------
-  document.addEventListener('DOMContentLoaded', function () {
-    var dropdownLinks = document.querySelectorAll('.dropdown-link');
+  * zenCSS Beta v2.0.0 (https://zencss.com/)
+  * Copyright 2022-2024 Shaun Mackey
+  * Licensed under MIT (https://github.com/zen-solutions/zencss/blob/main/LICENSE)
+  */
+
+//-------------------------------------
+// Dropdown
+//-------------------------------------
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdownLinks = document.querySelectorAll('.dropdown .dropdown-link');
 
     dropdownLinks.forEach(function(link) {
-        // Create and insert the caret
-        var caret = document.createElement('div');
-        caret.className = 'caret-up';
-        link.parentNode.insertBefore(caret, link.nextSibling);
-
         link.addEventListener('click', function (event) {
             event.preventDefault();
-            event.stopPropagation();
-            // Check if the current dropdown is already open
-            var isCurrentDropdownOpen = link.nextElementSibling.nextElementSibling.style.display === "block";
-            // Close all dropdowns
-            closeAllDropdowns();
-            // Toggle the current dropdown if it was not open
-            if (!isCurrentDropdownOpen) {
-                toggleDropdown(link.nextElementSibling.nextElementSibling, caret);
+
+            // Toggle the dropdown content visibility
+            var dropdownContent = link.nextElementSibling;
+            if (dropdownContent.style.display === 'block') {
+                dropdownContent.style.display = 'none';
+            } else {
+                // Close all open dropdowns before opening the new one
+                closeAllDropdowns();
+                dropdownContent.style.display = 'block';
             }
         });
     });
 });
 
-function toggleDropdown(dropdownContent, caret) {
-    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
-    caret.style.display = caret.style.display === "block" ? "none" : "block";
-}
-
 function closeAllDropdowns() {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var carets = document.getElementsByClassName("caret-up");
-    for (var i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].style.display = "none";
-        carets[i].style.display = "none";
-    }
+    var dropdowns = document.querySelectorAll('.dropdown .dropdown-content');
+    dropdowns.forEach(function(dropdown) {
+        dropdown.style.display = 'none';
+    });
 }
 
+// Close all dropdowns when clicking outside
 window.onclick = function(event) {
     if (!event.target.matches('.dropdown-link')) {
         closeAllDropdowns();
     }
 };
-
-
