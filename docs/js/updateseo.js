747eb972689e0690881d33fcce37c70a9f196ea4
“Shaun <djshamus@gmail.com>
2023-12-25 04:24:27 -0500

    seo + stability updates

---
+++ docs/js/updateseo.js
@@ -0,0 +1,43 @@
+/*
+  * zenCSS Beta v2.0.0 (https://zencss.com/)
+  * Copyright 2022-2024 Shaun Mackey
+  * Licensed under MIT (https://github.com/zen-solutions/zencss/blob/main/LICENSE)
+  */
+ 
+function updateSEO() {
+    const h1Element = document.querySelector('h1');
+    let descriptionContent;
+
+    // Check if there's an element with class 'lead'
+    const leadElement = document.querySelector('.lead');
+    if (leadElement) {
+        descriptionContent = leadElement.textContent;
+    } else if (h1Element) {
+        // If no 'lead' class, use the first <p> element after the <h1>
+        const nextPElement = h1Element.nextElementSibling;
+        if (nextPElement && nextPElement.tagName.toLowerCase() === 'p') {
+            descriptionContent = nextPElement.textContent;
+        }
+    }
+
+    // Update the title tag to "zenCSS - [h1 content]"
+    if (h1Element) {
+        document.title = `zenCSS - ${h1Element.textContent}`;
+    }
+
+    // Update the meta description
+    if (descriptionContent) {
+        const metaDescription = document.querySelector('meta[name="description"]');
+        if (metaDescription) {
+            metaDescription.setAttribute('content', descriptionContent);
+        } else {
+            // Create a new meta description tag if it doesn't exist
+            const newMetaDescription = document.createElement('meta');
+            newMetaDescription.setAttribute('name', 'description');
+            newMetaDescription.setAttribute('content', descriptionContent);
+            document.head.appendChild(newMetaDescription);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', updateSEO);
